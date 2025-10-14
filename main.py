b7bdf2419dc2a0b94a3dba8b8989cef2dde0df52
舟 <Robin.wangyuezhou@edu.yungu.org>
2025-10-14 20:17:31 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from http import *
+class MyHttp(Http):
+  def getRoot(self, path):
+    body = f"<h1>Hello</h1><p>Path={path}</p>"
+    self.response(body)
+
+  def get404(self, path):
+    body = f"<h1>404 Not Found</h1><p>Path={path}</p>"
+    self.response(body)
+
+MyHttp(HOST,PORT)
